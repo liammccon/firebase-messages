98bcb3170f2e2f4b7469d297b82585bcdd619b58
Liam McConlogue <liam.t.mccon@gmail.com>
2023-08-09 10:18:21 -0400

    Add Firebase configuration Copied the added lines from "Add SDK with npm" - make sure not to copy everything!

--- init-firebase.js
+++ init-firebase.js
@@ -2,9 +2,15 @@
  * @TODO replace the firebaseConfig object below with the one 
  * provided for you by firebase after creating your project!
 */
-const firebaseConfig = {
-    
-};
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+    apiKey: "AIzaSyDYGZDGCWXc_ek5HQ3vCaRugzI5H-t-Gtg",
+    authDomain: "firemessages-5e3b6.firebaseapp.com",
+    projectId: "firemessages-5e3b6",
+    storageBucket: "firemessages-5e3b6.appspot.com",
+    messagingSenderId: "109911068397",
+    appId: "1:109911068397:web:3fde225a15f5086278c6e4"
+  };
 
 // initialize the firebase app with this built-in function
 firebase.initializeApp(firebaseConfig);
